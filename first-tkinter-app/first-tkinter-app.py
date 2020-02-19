c214a4a39fe12a27f4304b34ede9264245ab4397
redsnowc <nuanyang.44@gmail.com>
2020-02-19 16:32:11 +0800

    edit

--- first-tkinter-app/first-tkinter-app.py
+++ first-tkinter-app/first-tkinter-app.py
@@ -1,18 +1,27 @@
-import tkinter as tk
+import tkinter
 from tkinter import ttk
 
 
 def greet():
-    print('Hello World!')
+    print(f'Hello {username.get() or "world"}!')
 
 
-root = tk.Tk()
+root = tkinter.Tk()
+
+username = tkinter.StringVar()
 
 ttk.Label(root, text="Hello World!", padding=(30, 10)).pack()
-greet_button = ttk.Button(root, text="Greet", command=greet)
-greet_button.pack()
 
-quit_button = ttk.Button(root, text="Quit", command=root.destroy)
-quit_button.pack()
+name_label = ttk.Label(root, text="用户名: ")
+name_label.pack(side="left", padx=(0, 10))
+name_entry = ttk.Entry(root, width=15, textvariable=username)
+name_entry.pack(side="left")
+name_entry.focus()
+
+greet_button = ttk.Button(root, text="你好", command=greet)
+greet_button.pack(side="left")
+
+quit_button = ttk.Button(root, text="退出", command=root.destroy)
+quit_button.pack(side="right")
 
 root.mainloop()
